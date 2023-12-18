0274d36243fa4e43b386b522cb8b68d3bb67d5bc
Ananda S <4833102+asarangaram@users.noreply.github.com>
2023-12-18 12:41:07 +0530

    support file image

--- app_loader/lib/src/providers/image.dart
+++ app_loader/lib/src/providers/image.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
 import 'dart:ui' as ui;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,17 +13,38 @@ class ImageNotifier extends StateNotifier<AsyncValue<ui.Image>> {
   Future<void> _get() async {
     state = await AsyncValue.guard(() async {
       try {
-        final data = await rootBundle.load(imagePath);
-        ui.Codec codec =
-            await ui.instantiateImageCodec(data.buffer.asUint8List());
-        ui.FrameInfo fi = await codec.getNextFrame();
-        ui.Image uiImage = fi.image;
-
-        return uiImage;
+        if (imagePath.startsWith('/')) {
+          return await imageFromFile;
+        }
+        if (imagePath.startsWith('assets')) {
+          return await imageFromAssets;
+        }
+        throw UnimplementedError();
       } catch (err) {
         throw Exception("Failed to load Image");
       }
     });
+  }
+
+  Future<ui.Image> get imageFromAssets async {
+    final data = await rootBundle.load(imagePath);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    ui.FrameInfo fi = await codec.getNextFrame();
+    ui.Image uiImage = fi.image;
+
+    return uiImage;
+  }
+
+  Future<ui.Image> get imageFromFile async {
+    File file = File(imagePath);
+    List<int> bytes = await file.readAsBytes();
+
+    // Decode the image from bytes
+    ui.Codec codec = await ui.instantiateImageCodec(Uint8List.fromList(bytes));
+    ui.FrameInfo fi = await codec.getNextFrame();
+
+    ui.Image uiImage = fi.image;
+    return uiImage;
   }
 }
 
