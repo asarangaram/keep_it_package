0e01cc4b221e958c03c62573cd1fb8e9abac7436
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-02-06 10:27:39 +0530

    close window

--- keep_it/lib/views/item_view.dart
+++ keep_it/lib/views/item_view.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:store/store.dart';
 
 import '../widgets/from_store/from_store.dart';
@@ -33,22 +34,43 @@ class ItemView extends ConsumerWidget {
     if (media.type.isFile && !File(media.path).existsSync()) {
       throw Exception('File not found ${media.path}');
     }
-    return Hero(
-      tag: '/item/${media.collectionId}/${media.id}',
-      child: File(media.path).existsSync()
-          ? switch (media) {
-              (final image) when image.type == CLMediaType.image => Image.file(
-                  File(image.path),
+    return Column(
+      children: [
+        if (context.canPop())
+          Align(
+            alignment: Alignment.topRight,
+            child: SizedBox(
+              height: 32 + 20,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16, right: 16, bottom: 16),
+                child: CLButtonIcon.small(
+                  Icons.close,
+                  onTap: context.pop,
                 ),
-              (final video) when video.type == CLMediaType.video =>
-                VideoPlayerScreen(
-                  path: video.path,
-                ),
-              _ => throw UnimplementedError(
-                  'Not yet implemented',
-                )
-            }
-          : const Text('Media not found'),
+              ),
+            ),
+          ),
+        Flexible(
+          child: Hero(
+            tag: '/item/${media.collectionId}/${media.id}',
+            child: File(media.path).existsSync()
+                ? switch (media) {
+                    (final image) when image.type == CLMediaType.image =>
+                      Image.file(
+                        File(image.path),
+                      ),
+                    (final video) when video.type == CLMediaType.video =>
+                      VideoPlayerScreen(
+                        path: video.path,
+                      ),
+                    _ => throw UnimplementedError(
+                        'Not yet implemented',
+                      )
+                  }
+                : const Text('Media not found'),
+          ),
+        ),
+      ],
     );
   }
 }
