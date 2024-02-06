3c1d2458701b373ae770af856bb53b712e8546bd
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-02-06 12:13:22 +0530

    descriptor

---
+++ app_loader/lib/src/cl_route_descriptor.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+typedef CLWidgetBuilder = Widget Function(
+  BuildContext context,
+  GoRouterState state,
+);
+
+@immutable
+class CLRouteDescriptor {
+  const CLRouteDescriptor({
+    required this.name,
+    required this.builder,
+  });
+  final String name;
+  final CLWidgetBuilder builder;
+}
+
+@immutable
+class CLShellRouteDescriptor extends CLRouteDescriptor {
+  final IconData iconData;
+  final String? label;
+  final bool isFullScreen;
+  const CLShellRouteDescriptor({
+    required super.name,
+    required super.builder,
+    required this.iconData,
+    required this.isFullScreen,
+    this.label,
+  });
+}
