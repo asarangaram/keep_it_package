7bf86885f461364701d9f0f4012591489381b40c
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-02-26 11:14:53 +0530

    use DeviceDirectories

--- store/lib/src/database/providers/db_providers.dart
+++ store/lib/src/database/providers/db_providers.dart
@@ -5,24 +5,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
 
+import '../../device/models/device_directories.dart';
+import '../../device/providers/device_directories.dart';
 import '../models/cl_media.dart';
 import '../models/collection.dart';
 import '../models/tag.dart';
 import 'db_manager.dart';
 import 'db_updater.dart';
 
-final _resourcesProvider = FutureProvider<(Database, String)>((ref) async {
+final _resourcesProvider =
+    FutureProvider<(Database, DeviceDirectories)>((ref) async {
   final updatedChanged = ref.watch(dbUpdaterNotifierProvider);
   final db = (await ref.watch(dbManagerProvider.future)).db;
-  final pathPrefix = (await getApplicationDocumentsDirectory()).path;
+  final docDir = await ref.watch(docDirProvider.future);
+
   _infoLogger('Refreshing resources');
-  return (db, pathPrefix);
+  return (db, docDir);
 });
 
 final getTagsByCollectionId =
     FutureProvider.family<Tags, int?>((ref, collectionId) async {
   final List<Tag> tags;
-  final (db, pathPrefix) = await ref.watch(_resourcesProvider.future);
+  final (db, _) = await ref.watch(_resourcesProvider.future);
   if (collectionId == null) {
     tags = TagDB.getAll(db);
   } else {
@@ -34,7 +38,7 @@ final getTagsByCollectionId =
 
 final getCollectionsByTagId =
     FutureProvider.family<Collections, int?>((ref, tagId) async {
-  final (db, pathPrefix) = await ref.watch(_resourcesProvider.future);
+  final (db, _) = await ref.watch(_resourcesProvider.future);
   final List<Collection> collections;
   final Tag? tag;
   if (tagId == null) {
@@ -50,24 +54,24 @@ final getCollectionsByTagId =
 
 final getCollectionById =
     FutureProvider.family<Collection, int>((ref, id) async {
-  final (db, pathPrefix) = await ref.watch(_resourcesProvider.future);
+  final (db, _) = await ref.watch(_resourcesProvider.future);
   _infoLogger('Loading Collection for id = $id');
   return CollectionDB.getById(db, id);
 });
 
 final getMediaByTagId =
     FutureProvider.family<List<CLMedia>, int>((ref, tagID) async {
-  final (db, pathPrefix) = await ref.watch(_resourcesProvider.future);
+  final (db, docDir) = await ref.watch(_resourcesProvider.future);
   _infoLogger('Loading Media by tag.id = $tagID');
-  return CLMediaDB.getByTagId(db, tagID, pathPrefix: pathPrefix);
+  return CLMediaDB.getByTagId(db, tagID, pathPrefix: docDir.docDir.path);
 });
 
 final getMediaByCollectionId =
     FutureProvider.family<CLMediaList, int>((ref, collectionId) async {
-  final (db, pathPrefix) = await ref.watch(_resourcesProvider.future);
+  final (db, docDir) = await ref.watch(_resourcesProvider.future);
   final collection = CollectionDB.getById(db, collectionId);
-  final entries =
-      CLMediaDB.getByCollectionId(db, collectionId, pathPrefix: pathPrefix);
+  final entries = CLMediaDB.getByCollectionId(db, collectionId,
+      pathPrefix: docDir.docDir.path);
   _infoLogger('Loading Media by collection.id = $collectionId');
   return CLMediaList(entries: entries, collection: collection);
 });
