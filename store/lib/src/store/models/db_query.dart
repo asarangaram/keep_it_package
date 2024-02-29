6319730e11ab68c52092dbc8439210b3487c9724
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-02-29 13:06:04 +0530

    Query to read from DB directly

--- store/lib/src/store/models/db_query.dart
+++ store/lib/src/store/models/db_query.dart
@@ -1,5 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:sqlite_async/sqlite_async.dart';
 
 @immutable
 class DBQuery<T> {
@@ -52,5 +53,14 @@ class DBQuery<T> {
         triggerOnTables.hashCode ^
         parameters.hashCode ^
         fromMap.hashCode;
+  }
+
+  // Use this only to pick specific items, not cached.
+  Future<List<T>> readMultiple(SqliteDatabase db) async {
+    return (await db.getAll(sql, parameters ?? [])).map(fromMap).toList();
+  }
+
+  Future<T?> read(SqliteDatabase db) async {
+    return (await db.getAll(sql, parameters ?? [])).map(fromMap).firstOrNull;
   }
 }
