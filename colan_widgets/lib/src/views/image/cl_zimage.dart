d67d2160a25ce4f339aa0677d2721c7965f8458b
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-03-15 16:58:22 +0530

    remove controller

--- colan_widgets/lib/src/views/image/cl_zimage.dart
+++ colan_widgets/lib/src/views/image/cl_zimage.dart
@@ -3,52 +3,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
-class CLzImage extends StatefulWidget {
+class CLzImage extends StatelessWidget {
   const CLzImage({required this.file, super.key});
   final File file;
 
   @override
-  State<CLzImage> createState() => _CLzImageState();
-}
-
-class _CLzImageState extends State<CLzImage> {
-  late final PhotoViewController controller;
-  double? scaleCopy;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = PhotoViewController()..outputStateStream.listen(listener);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  void listener(PhotoViewControllerValue value) {
-    setState(() {
-      scaleCopy = value.scale;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Positioned.fill(
-          child: PhotoView(
-            minScale: PhotoViewComputedScale.contained,
-            imageProvider: FileImage(widget.file),
-            controller: controller,
-          ),
-        ),
-        Text(
-          'Scale applied: $scaleCopy',
-          style: const TextStyle(color: Colors.white),
-        ),
-      ],
+    return PhotoView(
+      backgroundDecoration: const BoxDecoration(color: Colors.transparent),
+      minScale: PhotoViewComputedScale.contained,
+      imageProvider: FileImage(file),
     );
   }
 }
