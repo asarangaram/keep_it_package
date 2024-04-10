77cd00d4c4a8e3ccdadba620a3cfaa7e51d78a00
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-04-10 15:40:28 +0530

    all editing in image viewer - needs rework

--- colan_widgets/lib/src/views/image/cl_zimage.dart
+++ colan_widgets/lib/src/views/image/cl_zimage.dart
@@ -1,23 +1,27 @@
 import 'dart:io';
 
+import 'package:colan_widgets/colan_widgets.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class CLzImage extends StatefulWidget {
+class CLzImage extends ConsumerStatefulWidget {
   const CLzImage({required this.file, super.key, this.onLockPage});
   final File file;
   final void Function({required bool lock})? onLockPage;
 
   @override
-  State<CLzImage> createState() => _CLzImageState();
+  ConsumerState<CLzImage> createState() => _CLzImageState();
 }
 
-class _CLzImageState extends State<CLzImage> {
+class _CLzImageState extends ConsumerState<CLzImage> {
   bool isZooming = false;
+  bool isEditing = false;
 
   @override
   Widget build(BuildContext context) {
+    final showControl = ref.watch(showControlsProvider);
     return Stack(
       children: [
         Positioned.fill(
@@ -37,13 +41,13 @@ class _CLzImageState extends State<CLzImage> {
                       details.totalScale! <= 1.0) {
                     if (isZooming) {
                       isZooming = false;
-                      widget.onLockPage?.call(lock: isZooming);
+                      widget.onLockPage?.call(lock: false);
                     }
                   } else {
                     setState(() {
                       if (!isZooming) {
                         isZooming = true;
-                        widget.onLockPage?.call(lock: isZooming);
+                        widget.onLockPage?.call(lock: true);
                       }
                     });
                   }
@@ -52,6 +56,39 @@ class _CLzImageState extends State<CLzImage> {
             },
           ),
         ),
+        if (showControl)
+          Positioned(
+            top: 8,
+            left: 8,
+            child: Row(
+              children: [
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onBackground
+                        .withAlpha(192), // Color for the circular container
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: CLButtonIcon.verySmall(
+                      MdiIcons.pencil,
+                      color: Theme.of(context)
+                          .colorScheme
+                          .background
+                          .withAlpha(192),
+                      onTap: () {
+                        setState(() {
+                          isEditing = true;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
       ],
     );
   }
