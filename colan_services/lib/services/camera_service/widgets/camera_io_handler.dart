a36beec1b20c695d07c01f232a5fe53cef76f32e
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-04-23 13:34:40 +0530

    SAVE media immediately if collection is available

--- colan_services/lib/services/camera_service/widgets/camera_io_handler.dart
+++ colan_services/lib/services/camera_service/widgets/camera_io_handler.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
+
+import 'package:colan_services/colan_services.dart';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:store/store.dart';
 
-
-import '../../notification_services/provider/notify.dart';
-import '../../preview_service/view/preview.dart';
 import '../providers/captured_media.dart';
 
 class CameraIOHandler extends ConsumerWidget {
@@ -18,7 +19,7 @@ class CameraIOHandler extends ConsumerWidget {
 
   final Collection? collection;
   final Widget Function({
-    required void Function(String, {required bool isVideo}) onCapture,
+    required Future<void> Function(String, {required bool isVideo}) onCapture,
     required void Function(String message, {required dynamic error})? onError,
     required Widget previewWidget,
   }) builder;
@@ -29,6 +30,71 @@ class CameraIOHandler extends ConsumerWidget {
     required List<CLMedia> entries,
     Collection? collection,
   }) onReceiveCapturedMedia;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GetDBManager(
+      builder: (dbManager) {
+        return SafeExit(
+          onDone: onDone,
+          child: builder(
+            onError: (message, {required error}) {
+              ref
+                  .read(notificationMessageProvider.notifier)
+                  .push('$message: $error');
+            },
+            onCapture: (path, {required isVideo}) async {
+              final md5String = await File(path).checksum;
+              CLMedia? media = CLMedia(
+                path: path,
+                type: isVideo ? CLMediaType.video : CLMediaType.image,
+                collectionId: collection?.id,
+                md5String: md5String,
+              );
+              if (collection != null) {
+                media = await dbManager.upsertMedia(
+                  collectionId: collection!.id!,
+                  media: media,
+                  onPrepareMedia: (m, {required targetDir}) async {
+                    final updated =
+                        (await m.moveFile(targetDir: targetDir)).getMetadata();
+                    return updated;
+                  },
+                );
+                if (media == null) {
+                  throw Exception('Failed to store Media');
+                } else if (!File(media.path).existsSync()) {
+                  print('Soemthing seems wrong here!!!');
+                }
+              }
+
+              ref.read(capturedMediaProvider.notifier).add(media);
+            },
+            previewWidget: PreviewCapturedMedia(
+              sendMedia: (mediaList) async {
+                if (collection == null) {
+                  await onReceiveCapturedMedia(
+                    context,
+                    ref,
+                    entries: mediaList,
+                    collection: collection,
+                  );
+                }
+                ref.read(capturedMediaProvider.notifier).clear();
+                onDone();
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SafeExit extends ConsumerWidget {
+  const SafeExit({required this.onDone, required this.child, super.key});
+  final VoidCallback onDone;
+  final Widget child;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -62,41 +128,28 @@ class CameraIOHandler extends ConsumerWidget {
           }
         }
       },
-      child: builder(
-        onError: (message, {required error}) {
-          ref
-              .read(notificationMessageProvider.notifier)
-              .push('$message: $error');
-        },
-        onCapture: (path, {required isVideo}) {
-          ref.read(capturedMediaProvider.notifier).add(
-                CLMedia(
-                  path: path,
-                  type: isVideo ? CLMediaType.video : CLMediaType.image,
-                ),
-              );
-        },
-        previewWidget: InkWell(
-          onTap: () async {
-            await onReceiveCapturedMedia(
-              context,
-              ref,
-              entries: capturedMedia,
-              collection: collection,
-            );
-            ref.read(capturedMediaProvider.notifier).clear();
-            onDone();
-          },
-          child: capturedMedia.isEmpty
-              ? Container()
-              : CapturedMediaDecorator(
-                  child: PreviewService(
-                    media: capturedMedia.last,
-                    keepAspectRatio: false,
-                  ),
-                ),
-        ),
-      ),
+      child: child,
+    );
+  }
+}
+
+class PreviewCapturedMedia extends ConsumerWidget {
+  const PreviewCapturedMedia({required this.sendMedia, super.key});
+  final Future<void> Function(List<CLMedia>) sendMedia;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final capturedMedia = ref.watch(capturedMediaProvider);
+    return InkWell(
+      onTap: () => sendMedia(capturedMedia),
+      child: capturedMedia.isEmpty
+          ? Container()
+          : CapturedMediaDecorator(
+              child: PreviewService(
+                media: capturedMedia.last,
+                keepAspectRatio: false,
+              ),
+            ),
     );
   }
 }
