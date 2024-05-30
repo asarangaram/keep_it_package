6ac66abbde23f824216b60d0d9febaaa966549fb
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-05-30 13:40:45 +0530

    move migration to separate file

--- store/lib/src/store/models/m2_db_manager.dart
+++ store/lib/src/store/models/m2_db_manager.dart
@@ -5,6 +5,7 @@ import 'package:colan_widgets/colan_widgets.dart';
 import 'package:sqlite_async/sqlite3.dart';
 import 'package:sqlite_async/sqlite_async.dart';
 
+import 'm2_db_migration.dart';
 import 'm3_db_reader.dart';
 import 'm4_db_writer.dart';
 
@@ -194,50 +195,3 @@ extension ExtSqliteDatabase on SqliteDatabase {
     }
   }
 }
-
-final migrations = SqliteMigrations()
-  ..add(
-    SqliteMigration(1, (tx) async {
-      await tx.execute('''
-      CREATE TABLE IF NOT EXISTS Collection (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT,
-        label TEXT NOT NULL UNIQUE,
-        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    ''');
-      await tx.execute('''
-      CREATE TRIGGER IF NOT EXISTS update_dates_on_collection
-        AFTER UPDATE ON Collection
-        BEGIN
-            UPDATE Collection
-            SET updatedDate = CURRENT_TIMESTAMP
-            WHERE id = NEW.id;
-        END;
-    ''');
-      await tx.execute('''
-      CREATE TABLE IF NOT EXISTS Item (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        path TEXT NOT NULL UNIQUE,
-        ref TEXT,
-        collectionId INTEGER,
-        type TEXT NOT NULL,
-        md5String TEXT NOT NULL,
-        originalDate DATETIME,
-        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (collectionId) REFERENCES Collection(id)
-      )
-    ''');
-      await tx.execute('''
-      CREATE TRIGGER IF NOT EXISTS update_dates_on_item
-        AFTER UPDATE ON Item
-        BEGIN
-            UPDATE Item
-            SET updatedDate = CURRENT_TIMESTAMP
-            WHERE id = NEW.id;
-        END;
-    ''');
-    }),
-  );
---
+++ store/lib/src/store/models/m2_db_migration.dart
@@ -0,0 +1,48 @@
+import 'package:sqlite_async/sqlite_async.dart';
+
+final migrations = SqliteMigrations()
+  ..add(
+    SqliteMigration(1, (tx) async {
+      await tx.execute('''
+      CREATE TABLE IF NOT EXISTS Collection (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        label TEXT NOT NULL UNIQUE,
+        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
+      )
+    ''');
+      await tx.execute('''
+      CREATE TRIGGER IF NOT EXISTS update_dates_on_collection
+        AFTER UPDATE ON Collection
+        BEGIN
+            UPDATE Collection
+            SET updatedDate = CURRENT_TIMESTAMP
+            WHERE id = NEW.id;
+        END;
+    ''');
+      await tx.execute('''
+      CREATE TABLE IF NOT EXISTS Item (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        path TEXT NOT NULL UNIQUE,
+        ref TEXT,
+        collectionId INTEGER,
+        type TEXT NOT NULL,
+        md5String TEXT NOT NULL,
+        originalDate DATETIME,
+        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (collectionId) REFERENCES Collection(id)
+      )
+    ''');
+      await tx.execute('''
+      CREATE TRIGGER IF NOT EXISTS update_dates_on_item
+        AFTER UPDATE ON Item
+        BEGIN
+            UPDATE Item
+            SET updatedDate = CURRENT_TIMESTAMP
+            WHERE id = NEW.id;
+        END;
+    ''');
+    }),
+  );
