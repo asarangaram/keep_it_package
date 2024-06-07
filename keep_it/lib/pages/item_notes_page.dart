3d2e9bf277b6936e5a7280b5d6f2a5ba18eb1acf
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-06-07 12:13:19 +0530

    pop to swipe

--- keep_it/lib/pages/item_notes_page.dart
+++ keep_it/lib/pages/item_notes_page.dart
@@ -2,6 +2,7 @@ import 'package:app_loader/app_loader.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:store/store.dart';
 
@@ -25,29 +26,40 @@ class ItemNotesPage extends ConsumerWidget {
       id: id,
       buildOnData: (media) {
         return FullscreenLayout(
-          child: Column(
-            children: [
-              Expanded(
-                child: MediaViewer(
-                  media: media,
-                  onLockPage: ({required lock}) {},
-                  autoStart: false,
+          child: GestureDetector(
+            onHorizontalDragEnd: (details) {
+              if (details.primaryVelocity == null) return;
+              // pop on Swipe
+              if (details.primaryVelocity! > 0) {
+                if (context.canPop()) {
+                  context.pop();
+                }
+              }
+            },
+            child: Column(
+              children: [
+                Expanded(
+                  child: MediaViewer(
+                    media: media,
+                    onLockPage: ({required lock}) {},
+                    autoStart: false,
+                  ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8),
-                child: Divider(
-                  height: 2,
-                  thickness: 3,
-                  indent: 4,
-                  endIndent: 4,
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Divider(
+                    height: 2,
+                    thickness: 3,
+                    indent: 4,
+                    endIndent: 4,
+                  ),
                 ),
-              ),
-              const Expanded(
-                flex: 5,
-                child: NotesInput(),
-              ),
-            ],
+                const Expanded(
+                  flex: 5,
+                  child: NotesInput(),
+                ),
+              ],
+            ),
           ),
         );
       },
