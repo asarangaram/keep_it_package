912d3e0800d5f132241884f791d5d96ea6fa9854
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-06-26 12:24:58 +0530

    show when backup is availabe

---
+++ colan_services/lib/services/storage_monitor_service/views/storage_monitor.dart
@@ -0,0 +1,36 @@
+import 'package:colan_widgets/colan_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'storage_info_entry.dart';
+
+class StorageMonitor extends ConsumerWidget {
+  const StorageMonitor({required this.appSettings, super.key});
+  final AppSettings appSettings;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final persistentDirs = CLStandardDirectories.values
+        .where((stddir) => stddir.isStore)
+        .map(appSettings.directories.standardDirectory)
+        .toList();
+    final cacheDir = CLStandardDirectories.values
+        .where((stddir) => !stddir.isPersistent)
+        .map(appSettings.directories.standardDirectory)
+        .toList();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StorageInfoEntry(
+          label: 'Storage Used',
+          dirs: persistentDirs,
+        ),
+        StorageInfoEntry(
+          label: 'Cache',
+          dirs: cacheDir,
+          actions: const [CLMenuItem(title: 'Clear', icon: Icons.delete)],
+        ),
+      ],
+    );
+  }
+}
--- keep_it/lib/pages/settings_main.dart
+++ keep_it/lib/pages/settings_main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:colan_services/colan_services.dart';
 import 'package:colan_widgets/colan_widgets.dart';
@@ -7,22 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 import 'package:store/store.dart';
-
-@immutable
-class DirInfo {
-  const DirInfo({
-    required this.name,
-    required this.directory,
-    required this.statsAsync,
-    this.onTapAction,
-  });
-  final String name;
-  final String directory;
-  final VoidCallback? onTapAction;
-  final AsyncValue<StorageStatistics> statsAsync;
-}
 
 class SettingsMainPage extends ConsumerWidget {
   const SettingsMainPage({super.key});
@@ -49,8 +33,8 @@ class SettingsMainPage extends ConsumerWidget {
                         ),
                         title: Text('Deleted Items (${deletedMedia.length})'),
                       ),
-                    const StorageInfo(),
-                    const BackupView(),
+                    StorageMonitor(appSettings: appSettings),
+                    BackupView(appSettings: appSettings),
                   ],
                 );
               },
@@ -59,155 +43,5 @@ class SettingsMainPage extends ConsumerWidget {
         );
       },
     );
-  }
-}
-
-class StorageInfo extends ConsumerWidget {
-  const StorageInfo({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return GetAppSettings(
-      builder: (appSettings) {
-        final persistentDirs = CLStandardDirectories.values
-            .where((stddir) => stddir.isPersistent)
-            .map(appSettings.directories.standardDirectory)
-            .toList();
-        final cacheDir = CLStandardDirectories.values
-            .where((stddir) => !stddir.isPersistent)
-            .map(appSettings.directories.standardDirectory)
-            .toList();
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            StorageInfoEntry(
-              label: 'Storage Used',
-              dirs: persistentDirs,
-              actions: [
-                CLMenuItem(
-                  title: 'Archive',
-                  icon: Icons.archive,
-                  onTap: () async {
-                    return false;
-                  },
-                ),
-              ],
-            ),
-            StorageInfoEntry(
-              label: 'Cache',
-              dirs: cacheDir,
-              actions: const [CLMenuItem(title: 'Clear', icon: Icons.delete)],
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class StorageInfoEntry extends ConsumerWidget {
-  const StorageInfoEntry({
-    required this.label,
-    required this.dirs,
-    super.key,
-    this.actions,
-  });
-  final String label;
-  final List<CLDirectory> dirs;
-  final List<CLMenuItem>? actions;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final infoListAsync = dirs
-        .map(
-          (dir) => ref.watch(dir.infoStream).whenOrNull(data: (data) => data),
-        )
-        .toList();
-    CLDirectoryInfo? info;
-    Widget? trailing;
-    final infoReady = infoListAsync.every((element) => element != null);
-    if (infoReady) {
-      info = infoListAsync.reduce((a, b) => a! + b!);
-      trailing = (actions?.isEmpty ?? true)
-          ? null
-          : ElevatedButton.icon(
-              onPressed: actions![0].onTap,
-              label: Text(actions![0].title),
-              icon: Icon(actions![0].icon),
-            );
-    }
-    return ListTile(
-      title: Text(label),
-      subtitle: Text(info?.statistics ?? ''),
-      trailing: trailing,
-    );
-  }
-}
-
-class BackupView extends ConsumerStatefulWidget {
-  const BackupView({super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _BackupViewState();
-}
-
-class _BackupViewState extends ConsumerState<BackupView> {
-  @override
-  Widget build(BuildContext context) {
-    return GetAppSettings(
-      builder: (appSettings) {
-        return const BackupWatch();
-      },
-    );
-  }
-}
-
-class BackupWatch extends ConsumerWidget {
-  const BackupWatch({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final backupStatus = ref.watch(backupNowProvider);
-    return backupStatus.whenOrNull(
-          loading: () {
-            return const Center(child: CircularProgressIndicator());
-          },
-          error: (error, stackTrace) {
-            return Center(child: Text('Error while processing backup. $error'));
-          },
-          data: (progress) {
-            if (progress.isDone) {
-              return ListTile(
-                title: const Text('Backup'),
-                trailing: ElevatedButton(
-                  onPressed: () async {
-                    ref.read(refreshProvider.notifier).state++;
-                  },
-                  child: const Text('Backup Now'),
-                ),
-                subtitle: const Text(
-                  'This will remove earlier backup '
-                  'and create a new backup.',
-                ),
-              );
-            }
-            return Padding(
-              padding: const EdgeInsets.all(15),
-              child: LinearPercentIndicator(
-                width: MediaQuery.of(context).size.width - 50,
-                animation: true,
-                lineHeight: 20,
-                animationDuration: 2000,
-                percent: progress.fractCompleted,
-                animateFromLastPercent: true,
-                center: Text(progress.percentageAsText),
-                barRadius: const Radius.elliptical(5, 15),
-                progressColor: Theme.of(context).colorScheme.primary,
-                maskFilter: const MaskFilter.blur(BlurStyle.solid, 3),
-              ),
-            );
-          },
-        ) ??
-        Container();
   }
 }
