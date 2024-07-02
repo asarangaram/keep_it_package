5fddf25467f33adcd314c76a3b680de53d75f256
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-07-02 18:47:27 +0530

    Warnings

--- keep_it/lib/pages/stale_media_page.dart
+++ keep_it/lib/pages/stale_media_page.dart
@@ -1,22 +1,12 @@
-import 'dart:math';
-
 import 'package:app_loader/app_loader.dart';
-import 'package:colan_services/colan_services.dart';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keep_it/widgets/empty_state.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 
 import 'package:store/store.dart';
 
-import '../models/media_handler.dart';
-import '../modules/shared_media/step4_save_collection.dart';
-import '../modules/shared_media/wizard_page.dart';
 import '../modules/universal_media_handler/universal_media_handler.dart';
-
-import '../widgets/editors/collection_editor_wizard/create_collection_wizard.dart';
 
 class StaleMediaPage extends ConsumerWidget {
   const StaleMediaPage({super.key});
