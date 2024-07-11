cc5b0c62f41f84966d1b132186524a59dcb62e6d
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-07-11 13:50:27 +0530

    Warnings

--- keep_it/lib/pages/settings_main_page.dart
+++ keep_it/lib/pages/settings_main_page.dart
@@ -2,7 +2,6 @@ import 'package:colan_widgets/colan_widgets.dart';
 import 'package:device_resources/device_resources.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:keep_it/widgets/store_manager.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:store/store.dart';
--- store/lib/src/modules/media_wizard_service/views/wizard_preview.dart
+++ store/lib/src/modules/media_wizard_service/views/wizard_preview.dart
@@ -1,7 +1,6 @@
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../store/widgets/w3_get_media.dart';
 
