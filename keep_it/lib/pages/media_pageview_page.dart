7bbc1ed5de557cdf17844167a7eb05341d4061e9
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-07-12 14:14:14 +0530

    Media Fullscreen without safe area

--- keep_it/lib/pages/media_pageview_page.dart
+++ keep_it/lib/pages/media_pageview_page.dart
@@ -24,6 +24,7 @@ class MediaPageViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FullscreenLayout(
+      useSafeArea: false,
       child: StoreManager(
         builder: ({required storeAction}) {
           if (collectionId == null) {
