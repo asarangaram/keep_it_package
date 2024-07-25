c69d220b924bf0b13b6cb1ee8775ea9f678090d3
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-07-25 17:38:50 +0530

    Fix: problem with zoom and flickering

--- colan_services/lib/services/media_view_service/widgets/media_view.dart
+++ colan_services/lib/services/media_view_service/widgets/media_view.dart
@@ -36,7 +36,6 @@ class MediaView extends ConsumerStatefulWidget {
 }
 
 class _MediaViewState extends ConsumerState<MediaView> {
-  late CLMedia media;
   @override
   void initState() {
     super.initState();
@@ -44,95 +43,88 @@ class _MediaViewState extends ConsumerState<MediaView> {
 
   @override
   Widget build(BuildContext context) {
+    final media = widget.media;
     final ac = widget.actionControl;
 
-    return GetMedia(
-      id: widget.media.id!,
-      buildOnData: (media0) {
-        if (media0 == null) return Container();
-        final media = media0;
-        return GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () => ref.read(showControlsProvider.notifier).toggleControls(),
-          child: Stack(
-            children: [
-              const MediaBackground(),
-              Positioned.fill(
-                child: Hero(
-                  tag: '${widget.parentIdentifier} /item/${media.id}',
-                  child: MediaViewerRaw(
-                    media: media,
-                    autoStart: widget.autoStart,
-                    autoPlay: widget.autoPlay,
-                    onLockPage: widget.onLockPage,
-                    isLocked: widget.isLocked,
-                    getPreview: widget.getPreview,
-                  ),
-                ),
-              ),
-              MediaControls(
-                onMove: ac.onMove(
-                  () => TheStore.of(context).openWizard(
-                    context,
-                    [media],
-                    UniversalMediaSource.move,
-                  ),
-                ),
-                onDelete: ac.onDelete(() async {
-                  final confirmed = await ConfirmAction.deleteMediaMultiple(
-                        context,
-                        media: [media],
-                      ) ??
-                      false;
-                  if (!confirmed) return confirmed;
-                  if (context.mounted) {
-                    return TheStore.of(context).deleteMediaMultiple(
-                      context,
-                      [media],
-                    );
-                  }
-                  return false;
-                }),
-                onShare: ac.onShare(
-                  () =>
-                      TheStore.of(context).shareMediaMultiple(context, [media]),
-                ),
-                onEdit: (media.type == CLMediaType.video &&
-                        !VideoEditServices.isSupported)
-                    ? null
-                    : ac.onEdit(
-                        () async {
-                          final updatedMedia =
-                              await TheStore.of(context).openEditor(
-                            context,
-                            media,
-                            canDuplicateMedia: ac.canDuplicateMedia,
-                          );
-                          if (updatedMedia != media && context.mounted) {
-                            setState(() {
-                              //media = updatedMedia;
-                            });
-                          }
-
-                          return true;
-                        },
-                      ),
-                onPin: ac.onPin(
-                  () async {
-                    final res = await TheStore.of(context)
-                        .togglePinMultiple(context, [media]);
-                    if (res) {
-                      setState(() {});
-                    }
-                    return res;
-                  },
-                ),
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => ref.read(showControlsProvider.notifier).toggleControls(),
+      child: Stack(
+        children: [
+          const MediaBackground(),
+          Positioned.fill(
+            child: Hero(
+              tag: '${widget.parentIdentifier} /item/${media.id}',
+              child: MediaViewerRaw(
                 media: media,
+                autoStart: widget.autoStart,
+                autoPlay: widget.autoPlay,
+                onLockPage: widget.onLockPage,
+                isLocked: widget.isLocked,
+                getPreview: widget.getPreview,
               ),
-            ],
+            ),
           ),
-        );
-      },
+          MediaControls(
+            onMove: ac.onMove(
+              () => TheStore.of(context).openWizard(
+                context,
+                [media],
+                UniversalMediaSource.move,
+              ),
+            ),
+            onDelete: ac.onDelete(() async {
+              final confirmed = await ConfirmAction.deleteMediaMultiple(
+                    context,
+                    media: [media],
+                  ) ??
+                  false;
+              if (!confirmed) return confirmed;
+              if (context.mounted) {
+                return TheStore.of(context).deleteMediaMultiple(
+                  context,
+                  [media],
+                );
+              }
+              return false;
+            }),
+            onShare: ac.onShare(
+              () => TheStore.of(context).shareMediaMultiple(context, [media]),
+            ),
+            onEdit: (media.type == CLMediaType.video &&
+                    !VideoEditServices.isSupported)
+                ? null
+                : ac.onEdit(
+                    () async {
+                      final updatedMedia =
+                          await TheStore.of(context).openEditor(
+                        context,
+                        media,
+                        canDuplicateMedia: ac.canDuplicateMedia,
+                      );
+                      if (updatedMedia != media && context.mounted) {
+                        setState(() {
+                          //media = updatedMedia;
+                        });
+                      }
+
+                      return true;
+                    },
+                  ),
+            onPin: ac.onPin(
+              () async {
+                final res = await TheStore.of(context)
+                    .togglePinMultiple(context, [media]);
+                if (res) {
+                  setState(() {});
+                }
+                return res;
+              },
+            ),
+            media: media,
+          ),
+        ],
+      ),
     );
   }
 }
