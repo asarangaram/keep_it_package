8150dbb986a69bb27a1383abddacb335c7bd925c
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-01 16:32:00 +0530

    fix: flutter/issues/148335 - video file extension

--- colan_services/lib/services/camera_service/cl_camera_service.dart
+++ colan_services/lib/services/camera_service/cl_camera_service.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:cl_camera/cl_camera.dart';
 import 'package:colan_services/colan_services.dart';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:path/path.dart' as p;
 import 'providers/captured_media.dart';
 import 'widgets/get_cameras.dart';
 import 'widgets/preview.dart';
@@ -46,7 +48,20 @@ class CLCameraService extends ConsumerWidget {
           },
           cameras: cameras,
           onCapture: (file, {required isVideo}) async {
-            final media = await onNewMedia(file, isVideo: isVideo);
+            String? updatedFile;
+            if (isVideo) {
+              /// Refer https://github.com/flutter/flutter/issues/148335
+              /// android_camerax plugin returns .temp extension
+              /// for recorded video
+              final currentExtension = p.extension(file);
+              if (currentExtension.toLowerCase() != '.mp4') {
+                updatedFile = p.setExtension(file, '.mp4');
+                File(file).copySync(updatedFile);
+                File(file).deleteSync();
+              }
+            }
+            final media =
+                await onNewMedia(updatedFile ?? file, isVideo: isVideo);
             if (media != null) {
               ref.read(capturedMediaProvider.notifier).add(media);
             }
