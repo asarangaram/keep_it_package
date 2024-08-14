9034128cae7a56256d4d142c64a9c523d1a4e882
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-14 11:47:05 +0530

    colan_cmdline

---
+++ colan_cmdline/example/colan_cmdline_example.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:colan_cmdline/colan_cmdline.dart';
+import 'package:json_diff/json_diff.dart';
+
+void main() async {
+  final server = await CLServer(name: 'udesktop.local', port: 5000).withId;
+  print(server);
+  if (server != null) {
+    final String? collectionsPrev;
+    final collectionStore = File('data.json');
+    if (collectionStore.existsSync()) {
+      collectionsPrev = collectionStore.readAsStringSync();
+    } else {
+      collectionsPrev = null;
+    }
+    final collections = await server.getEndpoint('/collection');
+    //print(collections);
+    //print(collectionsPrev);
+    if (collectionsPrev != null) {
+      final differ = JsonDiffer(collectionsPrev, collections);
+      DiffNode diff = differ.diff();
+      print(diff.added);
+      print(diff.removed);
+      print(diff.changed);
+      print(diff.moved);
+      for (final n in diff.node.entries) {
+        print(n.value.added);
+        print(n.value.removed);
+        print(n.value.changed);
+        print(n.value.moved);
+      }
+    }
+
+    {
+      var file = File('data.json');
+      await file.writeAsString(collections);
+    }
+  }
+}
+
+
+/*
+import 'package:json_diff/json_diff.dart';
+
+void main() {
+  var json1 = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+  };
+  
+  var json2 = {
+    "name": "John",
+    "age": 31,
+    "city": "New York"
+  };
+
+  var diff = JsonDiff.diff(json1, json2);
+  print(diff);
+}
+*/
