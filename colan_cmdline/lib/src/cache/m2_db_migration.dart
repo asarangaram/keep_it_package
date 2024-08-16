7724feda7f728493628a234a92cf9f5bc39c9844
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-16 13:00:15 +0530

    modified tables to fit server

--- colan_cmdline/lib/src/cache/m2_db_migration.dart
+++ colan_cmdline/lib/src/cache/m2_db_migration.dart
@@ -5,88 +5,41 @@ final migrations = SqliteMigrations()
     SqliteMigration(1, (tx) async {
       await tx.execute('''
       CREATE TABLE IF NOT EXISTS Collection (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT,
-        label TEXT NOT NULL UNIQUE,
-        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    ''');
-      await tx.execute('''
-      CREATE TRIGGER IF NOT EXISTS update_dates_on_collection
-        AFTER UPDATE ON Collection
-        BEGIN
-            UPDATE Collection
-            SET updatedDate = CURRENT_TIMESTAMP
-            WHERE id = NEW.id;
-        END;
+          id INTEGER NOT NULL PRIMARY KEY,  -- Not auto-incremented
+          label TEXT NOT NULL UNIQUE,
+          createdDate DATETIME NOT NULL,
+          updatedDate DATETIME NOT NULL,
+          description TEXT,
+          isDirty INTEGER DEFAULT TRUE
+      );
     ''');
       await tx.execute('''
       CREATE TABLE IF NOT EXISTS Item (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER NOT NULL UNIQUE,
         path TEXT NOT NULL UNIQUE,
         ref TEXT,
         collectionId INTEGER,
         type TEXT NOT NULL,
         md5String TEXT NOT NULL,
         originalDate DATETIME,
-        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        createdDate DATETIME NOT NULL,
+        updatedDate DATETIME NOT NULL,
+        isDeleted INTEGER DEFAULT 0,
+        isHidden INTEGER DEFAULT 0,
+        pin TEXT DEFAULT NULL,
+        isDirty INTEGER DEFAULT TRUE,
         FOREIGN KEY (collectionId) REFERENCES Collection(id)
       )
     ''');
       await tx.execute('''
-      CREATE TRIGGER IF NOT EXISTS update_dates_on_item
-        AFTER UPDATE ON Item
-        BEGIN
-            UPDATE Item
-            SET updatedDate = CURRENT_TIMESTAMP
-            WHERE id = NEW.id;
-        END;
-    ''');
-    }),
-  )
-  ..add(
-    SqliteMigration(2, (tx) async {
-      await tx.execute(
-        'ALTER TABLE Item ADD COLUMN isDeleted INTEGER DEFAULT 0',
-      );
-      await tx.execute(
-        'ALTER TABLE Item ADD COLUMN isHidden INTEGER DEFAULT 0',
-      );
-      await tx.execute(
-        'ALTER TABLE Item ADD COLUMN isPinned INTEGER DEFAULT 0',
-      );
-      await tx.execute('UPDATE Item SET isDeleted = 0');
-      await tx.execute('UPDATE Item SET isHidden = 0');
-      await tx.execute('UPDATE Item SET isPinned = 0');
-    }),
-  )
-  ..add(
-    SqliteMigration(3, (tx) async {
-      await tx.execute('ALTER TABLE Item ADD COLUMN pin TEXT DEFAULT NULL');
-      await tx.execute('UPDATE Item SET pin = NULL');
-    }),
-  )
-  ..add(
-    SqliteMigration(4, (tx) async {
-      await tx.execute('''
       CREATE TABLE IF NOT EXISTS Notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER NOT NULL UNIQUE,
         path TEXT NOT NULL UNIQUE,
         type TEXT NOT NULL,
-        createdDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
+        createdDate DATETIME NOT NULL,
+        updatedDate DATETIME NOT NULL,
+        isDirty INTEGER DEFAULT TRUE
       )
-      ''');
-      await tx.execute('''
-      CREATE TRIGGER IF NOT EXISTS update_dates_on_notes
-        AFTER UPDATE ON Notes
-        BEGIN
-            UPDATE Notes
-            SET updatedDate = CURRENT_TIMESTAMP
-            WHERE id = NEW.id;
-        END;
       ''');
       await tx.execute('''
       CREATE TABLE IF NOT EXISTS ItemNote (
@@ -96,6 +49,6 @@ final migrations = SqliteMigrations()
         FOREIGN KEY (noteId) REFERENCES Notes(id) ON DELETE CASCADE,
         FOREIGN KEY (itemId) REFERENCES Item(id) ON DELETE CASCADE
       )
-    ''');
+      ''');
     }),
   );
