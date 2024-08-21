41b56e682d5abc2d5ae81a3438cefa3faa6d5172
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-21 10:18:57 +0530

    pullCollection

--- store/lib/store.dart
+++ store/lib/store.dart
@@ -9,4 +9,5 @@ export 'src/models/cl_media.dart';
 export 'src/models/cl_media_type.dart' show CLMediaType;
 export 'src/models/cl_note.dart';
 export 'src/models/collection.dart';
+export 'src/models/collections.dart';
 export 'src/models/store.dart';
