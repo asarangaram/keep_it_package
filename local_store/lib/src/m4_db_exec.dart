9587f8b718f9a86e87f8492bc72b0b94d15de698
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-21 10:19:07 +0530

    remove unused functions

--- local_store/lib/src/m4_db_exec.dart
+++ local_store/lib/src/m4_db_exec.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:collection/collection.dart';
+import 'package:local_store/local_store.dart';
 import 'package:meta/meta.dart';
 import 'package:sqlite_async/sqlite_async.dart';
 import 'package:store/store.dart';
 
 import 'db_utils/db_command.dart';
-import 'db_utils/db_command_batch.dart';
 
 @immutable
 class DBExec<T> {
@@ -18,35 +19,6 @@ class DBExec<T> {
   final String table;
   final Map<String, dynamic>? Function(T obj) toMap;
   final Future<T?> Function(SqliteWriteContext tx, T obj)? readBack;
-
-  DBExec<T> copyWith({
-    String? table,
-    Map<String, dynamic>? Function(T obj)? toMap,
-    Future<T?> Function(SqliteWriteContext tx, T obj)? readBack,
-  }) {
-    return DBExec<T>(
-      table: table ?? this.table,
-      toMap: toMap ?? this.toMap,
-      readBack: readBack ?? this.readBack,
-    );
-  }
-
-  @override
-  bool operator ==(covariant DBExec<T> other) {
-    if (identical(this, other)) return true;
-    final mapEquals = const DeepCollectionEquality().equals;
-
-    return other.table == table &&
-        mapEquals(other.toMap, toMap) &&
-        other.readBack == readBack;
-  }
-
-  @override
-  int get hashCode => table.hashCode ^ toMap.hashCode ^ readBack.hashCode;
-
-  @override
-  String toString() =>
-      'DBExec(table: $table, toMap: $toMap, readBack: $readBack)';
 
   Future<T?> upsert(
     SqliteWriteContext tx,
@@ -95,66 +67,6 @@ class DBExec<T> {
       }
     }
     _infoLogger('upsertNote: Done :  $result');
-    return result;
-  }
-
-  Future<T?> insert(
-    SqliteWriteContext tx,
-    T obj, {
-    bool ignore = false,
-  }) async {
-    final map = toMap(obj);
-    if (map == null) return null;
-
-    await DBCommand.insert(
-      map,
-      table: table,
-    ).execute(tx);
-
-    final result = await readBack?.call(tx, obj);
-    _infoLogger('Readback:  $result');
-    return result;
-  }
-
-  Future<T?> update(
-    SqliteWriteContext tx,
-    T obj, {
-    bool ignore = false,
-  }) async {
-    final map = toMap(obj);
-    if (map == null) return null;
-
-    await DBCommand.update(
-      map,
-      table: table,
-    ).execute(tx);
-
-    final result = await readBack?.call(tx, obj);
-    _infoLogger('Readback:  $result');
-    return result;
-  }
-
-  Future<List<T?>> upsertAll(
-    SqliteWriteContext tx,
-    List<T> objList, {
-    required Future<List<int>> Function(List<int>) getPresentIdList,
-  }) async {
-    final list = [...objList.map(toMap).where((e) => e != null).map((e) => e!)];
-
-    await (await DBBatchCommand.upsertAsync(
-      list,
-      table: table,
-      getPresentIdList: getPresentIdList,
-    ))
-        .execute(tx);
-
-    final result = <T?>[];
-    for (final obj in objList) {
-      result.add(
-        await readBack?.call(tx, obj),
-      );
-    }
-    _infoLogger('Readback:  $result');
     return result;
   }
 
