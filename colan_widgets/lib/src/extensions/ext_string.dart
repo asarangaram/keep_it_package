1ce33e2458704699842815a7fe4228fae4c71a91
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-08-28 13:34:57 +0530

    WIP [X]: colan_widgets: remove everything related to store

--- colan_widgets/lib/src/extensions/ext_string.dart
+++ colan_widgets/lib/src/extensions/ext_string.dart
@@ -1,35 +1,7 @@
 import 'dart:math';
 
-import '../app_logger.dart';
-
-extension ColonExtensionOnString on String {
-  bool isURL() {
-    try {
-      final uri = Uri.parse(this);
-      // Check if the scheme is non-empty to ensure it's a valid URL
-      return uri.scheme.isNotEmpty;
-    } catch (e) {
-      return false; // Parsing failed, not a valid URL
-    }
-  }
-
-  void printString({String prefix = ''}) {
-    _infoLogger('$prefix $this');
-  }
-
+extension StoreExtensionOnString on String {
   String uptoLength(int N) {
     return substring(0, min(length, N));
-  }
-
-  String capitalizeFirstLetter() {
-    if (isEmpty) return this; // Return the string as is if it's empty
-    return '${this[0].toUpperCase()}${substring(1)}';
-  }
-}
-
-bool _disableInfoLogger = true;
-void _infoLogger(String msg) {
-  if (!_disableInfoLogger) {
-    logger.i(msg);
   }
 }
