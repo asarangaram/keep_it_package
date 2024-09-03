9a6c44ca21c3d6c7166c9f13378a125f1d2b4147
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-09-03 17:34:11 +0530

    warnings

--- colan_services/lib/colan_services.dart
+++ colan_services/lib/colan_services.dart
@@ -17,11 +17,10 @@ export 'services/notification_services/notification_service.dart'
     show NotificationService;
 export 'services/notification_services/provider/notify.dart'
     show notificationMessageProvider;
-
-export 'services/storage_service/widgets/get_device_directories.dart'
-    show GetDeviceDirectories;
 export 'services/sharing_service/models/share_files.dart' show ShareManager;
 export 'services/storage_service/extensions/ext_file.dart' show ExtFileAnalysis;
+export 'services/storage_service/widgets/get_device_directories.dart'
+    show GetDeviceDirectories;
 export 'services/storage_service/widgets/storage_monitor.dart'
     show StorageMonitor;
 export 'services/store_service/store_service.dart';
