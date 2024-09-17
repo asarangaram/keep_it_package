0adb01112aebe99b088fc89b70498051d2fb03ac
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-09-17 16:51:19 +0530

    fix uploader collection label - Not yet complete

--- uploader.sh
+++ uploader.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 /path/to/your/folder"
+    echo "Usage: $0 </path/to/your/folder> <Collection Name>"
     exit 1
 fi
 
+
+
 folder_path="$1"
+collection_label="$2"
+
+echo $collection_label
 
 url="http://192.168.1.222:5000/media"
 
@@ -18,7 +23,7 @@ for file in "$folder_path"/*; do
         mime_type=$(file --mime-type -b "$file")
         
         echo curl -v  -F "media=@${file};type=${mime_type}" -F "collectionLabel=random"  "$url"
-        time curl -v  -F "media=@${file};type=${mime_type}" -F "collectionLabel=random"  "$url"
+        time curl -v  -F "media=@${file};type=${mime_type}" -F "collectionLabel=$collection_label"  "$url"
 
         sleep 1
 
