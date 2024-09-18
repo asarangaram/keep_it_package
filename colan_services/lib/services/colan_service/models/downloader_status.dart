46ff654590afab3bc80cd142d463225489a899b6
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-09-18 22:08:40 +0530

    to Map

--- colan_services/lib/services/colan_service/models/downloader_status.dart
+++ colan_services/lib/services/colan_service/models/downloader_status.dart
@@ -1,4 +1,7 @@
-import 'package:meta/meta.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 
 @immutable
 class DownloaderStatus {
@@ -53,4 +56,34 @@ class DownloaderStatus {
     // ignore: lines_longer_than_80_chars
     return 'DownloaderStatus(running: $running, waiting: $waiting, completed: $completed, unknown: $unknown)';
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'running': running.toDouble(),
+      'waiting': waiting.toDouble(),
+      'completed': completed.toDouble(),
+      'unknown': unknown.toDouble(),
+    };
+  }
+
+  Map<String, Color> get colorMap => <String, Color>{
+        'running': Colors.orange,
+        'waiting': Colors.white,
+        'completed': Colors.blue,
+        'unknown': Colors.black,
+      };
+
+  factory DownloaderStatus.fromMap(Map<String, dynamic> map) {
+    return DownloaderStatus(
+      running: (map['running'] as double).toInt(),
+      waiting: (map['waiting'] as double).toInt(),
+      completed: (map['completed'] as double).toInt(),
+      unknown: (map['unknown'] as double).toInt(),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory DownloaderStatus.fromJson(String source) =>
+      DownloaderStatus.fromMap(json.decode(source) as Map<String, dynamic>);
 }
