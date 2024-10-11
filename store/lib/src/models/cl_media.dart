e79a7d63cfc0ecc3339f1bf2aee3e210e55c5620
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-10-11 10:17:53 +0530

    fix: dates were incorrectly updated

--- store/lib/src/models/cl_media.dart
+++ store/lib/src/models/cl_media.dart
@@ -84,8 +84,8 @@ class CLMedia extends CLMediaBase {
       id: id,
       pin: pin,
       originalDate: originalDate,
-      updatedDate: createdDate ?? time,
-      createdDate: updatedDate ?? time,
+      createdDate: createdDate ?? time,
+      updatedDate: updatedDate ?? time,
     );
   }
 
@@ -201,7 +201,7 @@ class CLMedia extends CLMediaBase {
   @override
   String toString() {
     // ignore: lines_longer_than_80_chars
-    return 'CLMediaBase(id: $id, name: $name, type: $type, fExt: $fExt, ref: $ref, originalDate: $originalDate, createdDate: $createdDate, updatedDate: $updatedDate, md5String: $md5String, isDeleted: $isDeleted, isHidden: $isHidden, pin: $pin, collectionId: $collectionId, isAux: $isAux, isPreviewCached: $isPreviewCached, isMediaCached: $isMediaCached, previewLog: $previewLog, mediaLog: $mediaLog, isMediaOriginal: $isMediaOriginal, serverUID: $serverUID, isEdited: $isEdited, haveItOffline: $haveItOffline, mustDownloadOriginal: $mustDownloadOriginal)';
+    return 'CLMedia(id: $id, name: $name, type: $type, fExt: $fExt, ref: $ref, originalDate: $originalDate, createdDate: $createdDate, updatedDate: $updatedDate, md5String: $md5String, isDeleted: $isDeleted, isHidden: $isHidden, pin: $pin, collectionId: $collectionId, isAux: $isAux, isPreviewCached: $isPreviewCached, isMediaCached: $isMediaCached, previewLog: $previewLog, mediaLog: $mediaLog, isMediaOriginal: $isMediaOriginal, serverUID: $serverUID, isEdited: $isEdited, haveItOffline: $haveItOffline, mustDownloadOriginal: $mustDownloadOriginal)';
   }
 
   @override
