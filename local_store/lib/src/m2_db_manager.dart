87e8b917da3c7b15dbba135e1afb18f76262cd9e
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-11-06 13:19:09 +0530

    warnings

--- local_store/lib/src/m2_db_manager.dart
+++ local_store/lib/src/m2_db_manager.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:meta/meta.dart';
 import 'package:sqlite_async/sqlite_async.dart';
