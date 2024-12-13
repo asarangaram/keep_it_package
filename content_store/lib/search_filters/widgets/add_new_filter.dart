534ec1300dbed61374134ccf669a12b30eee808a
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-12-13 19:16:58 +0530

    aligment for AddNewFilter

--- content_store/lib/search_filters/widgets/add_new_filter.dart
+++ content_store/lib/search_filters/widgets/add_new_filter.dart
@@ -32,46 +32,54 @@ class AddNewFilterState extends ConsumerState<AddNewFilter> {
     final unusedFilters =
         ref.watch(filtersProvider.select((e) => e.unusedFilters));
     if (unusedFilters.isEmpty) return const SizedBox.shrink();
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CLText.small('Add a search option'),
-          const SizedBox(
-            width: 16,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: DropdownButton<CLFilter<CLMedia>>(
-              isDense: true,
-              onChanged: (CLFilter<CLMedia>? item) {
-                if (item != null) {
-                  ref.read(filtersProvider.notifier).addFilter(item);
-                  setState(() {
-                    currentValue = null;
-                  });
-                }
-              },
-              value: currentValue,
-              items: [
-                for (final filter in unusedFilters)
-                  DropdownMenuItem<CLFilter<CLMedia>>(
-                    value: filter,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        CLText.tiny(
-                          maxString,
-                          color: Colors.transparent,
-                        ),
-                        CLText.tiny(filter.name),
-                      ],
-                    ),
-                  ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const CLText.small('Add a search option'),
+            const SizedBox(
+              width: 16,
             ),
-          ),
-        ],
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: DropdownButton<CLFilter<CLMedia>>(
+                isDense: true,
+                elevation: 0,
+                alignment: AlignmentDirectional.center,
+                borderRadius: BorderRadius.zero,
+                padding: EdgeInsets.zero,
+                onChanged: (CLFilter<CLMedia>? item) {
+                  if (item != null) {
+                    ref.read(filtersProvider.notifier).addFilter(item);
+                    setState(() {
+                      currentValue = null;
+                    });
+                  }
+                },
+                value: currentValue,
+                items: [
+                  for (final filter in unusedFilters)
+                    DropdownMenuItem<CLFilter<CLMedia>>(
+                      value: filter,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CLText.tiny(
+                            maxString,
+                            color: Colors.transparent,
+                          ),
+                          CLText.tiny(filter.name),
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
