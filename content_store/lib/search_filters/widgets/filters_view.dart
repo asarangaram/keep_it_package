f059c2fd96aa2bedac2e40d1cc2938a99abbe238
Ananda S <4833102+asarangaram@users.noreply.github.com>
2025-01-08 12:12:06 +0530

    have maxsize for search filters

--- content_store/lib/search_filters/widgets/filters_view.dart
+++ content_store/lib/search_filters/widgets/filters_view.dart
@@ -1,20 +1,33 @@
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:store/store.dart';
 
+import '../models/filter/base_filter.dart';
 import '../providers/filters.dart';
 import 'add_new_filter.dart';
 import 'filter_view.dart';
 
 class SearchOptions extends ConsumerStatefulWidget {
-  const SearchOptions({super.key});
+  const SearchOptions({super.key, this.maxHeight = 300});
+  final double maxHeight;
 
   @override
-  ConsumerState<SearchOptions> createState() => _SearchOptionsState();
+  ConsumerState<ConsumerStatefulWidget> createState() => SearchOptionsState();
 }
 
-class _SearchOptionsState extends ConsumerState<SearchOptions> {
+class SearchOptionsState extends ConsumerState<SearchOptions> {
+  double? height;
+  GlobalKey globalKey = GlobalKey();
+  final ScrollController controller = ScrollController();
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   bool minimize = false;
+
   @override
   Widget build(BuildContext context) {
     final isExpanded = ref.watch(filtersProvider.select((e) => e.editing));
@@ -23,79 +36,140 @@ class _SearchOptionsState extends ConsumerState<SearchOptions> {
         filters.isNotEmpty &&
         filters.every((e) => e.enabled);
     final hide = !isExpanded && !hasFilter;
-    return AnimatedSwitcher(
-      duration: const Duration(milliseconds: 300),
-      switchInCurve: Curves.easeInOut,
-      switchOutCurve: Curves.easeInOut,
-      transitionBuilder: (Widget child, Animation<double> animation) =>
-          SizeTransition(
-        sizeFactor: animation,
-        child: child,
+    if (hide) {
+      return const SizedBox.shrink();
+    }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final renderBox =
+          globalKey.currentContext?.findRenderObject()! as RenderBox?;
+      final size = renderBox?.size;
+
+      if (size == null) {
+        setState(() {
+          height = 0;
+        });
+      } else if (height != size.height) {
+        height = size.height;
+        setState(() {});
+      }
+    });
+
+    return Card(
+      elevation: 8,
+      color: Colors.grey.shade100,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          2,
+        ), // Adjust the border radius as needed
       ),
-      child: hide
-          ? const SizedBox.shrink()
-          : Card(
-              elevation: 8,
-              color: Colors.grey.shade100,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  2,
-                ), // Adjust the border radius as needed
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+      margin: const EdgeInsets.only(bottom: 8),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        switchInCurve: Curves.easeInOut,
+        switchOutCurve: Curves.easeInOut,
+        transitionBuilder: (Widget child, Animation<double> animation) =>
+            SizeTransition(
+          sizeFactor: animation,
+          child: child,
+        ),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.grey.shade300),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(color: Colors.grey.shade300),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        if (hasFilter) ...[
-                          TextButton(
-                            onPressed: () {
-                              ref.read(filtersProvider.notifier).clearFilters();
-                            },
-                            child: const Text(
-                              'Clear Search',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                        ],
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: CLButtonIcon.tiny(
-                            !hasFilter
-                                ? Icons.close
-                                : minimize
-                                    ? Icons.arrow_drop_down_sharp
-                                    : Icons.arrow_drop_up_sharp,
-                            onTap: () {
-                              if (!hasFilter) {
-                                ref
-                                    .read(filtersProvider.notifier)
-                                    .disableEdit();
-                              } else {
-                                setState(() {
-                                  minimize = !minimize;
-                                });
-                              }
-                            },
-                          ),
+                  if (hasFilter) ...[
+                    TextButton(
+                      onPressed: () {
+                        ref.read(filtersProvider.notifier).clearFilters();
+                      },
+                      child: const Text(
+                        'Clear Search',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                      ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: CLButtonIcon.tiny(
+                      !hasFilter
+                          ? Icons.close
+                          : minimize
+                              ? Icons.arrow_drop_down_sharp
+                              : Icons.arrow_drop_up_sharp,
+                      onTap: () {
+                        if (!hasFilter) {
+                          ref.read(filtersProvider.notifier).disableEdit();
+                        } else {
+                          setState(() {
+                            minimize = !minimize;
+                          });
+                        }
+                      },
                     ),
                   ),
-                  if (!minimize && hasFilter)
-                    ...filters.map((filter) => FilterView(filter: filter)),
-                  if (!minimize) const AddNewFilter(),
                 ],
               ),
             ),
+            if (!minimize) const AddNewFilter(),
+            if (!minimize)
+              if (height == null || height! < widget.maxHeight)
+                SearchOptions0(
+                  key: globalKey,
+                  filters: filters,
+                )
+              else
+                SizedBox(
+                  height: widget.maxHeight,
+                  child: Scrollbar(
+                    controller: controller,
+                    child: SingleChildScrollView(
+                      controller: controller,
+                      child: SearchOptions0(
+                        key: globalKey,
+                        filters: filters,
+                      ),
+                    ),
+                  ),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchOptions0 extends ConsumerStatefulWidget {
+  const SearchOptions0({
+    super.key,
+    this.filters,
+  });
+  final List<CLFilter<CLMedia>>? filters;
+
+  @override
+  ConsumerState<SearchOptions0> createState() => SearchOptions0State();
+}
+
+class SearchOptions0State extends ConsumerState<SearchOptions0> {
+  @override
+  Widget build(BuildContext context) {
+    final filters = widget.filters;
+    final hasFilter = filters != null &&
+        filters.isNotEmpty &&
+        filters.every((e) => e.enabled);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (hasFilter) ...filters.map((filter) => FilterView(filter: filter)),
+      ],
     );
   }
 }
