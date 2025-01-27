a141f2c9d86bb6dadbdc2ec0cabda9f912687eaf
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-01-27 13:14:27 +0530

    script for outdated

---
+++ outdated.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "$SCRIPT_DIR" || exit
+
+for dir in "$SCRIPT_DIR"/*/
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    currdir="${dir##*/}"    # print everything after the final "/"
+    PUBSPEC="$SCRIPT_DIR/$currdir/pubspec.yaml"
+    if test -f "$PUBSPEC"; then
+        echo "$currdir is a flutter project"
+        pushd "$SCRIPT_DIR/$currdir" || exit
+        flutter pub outdated 
+        popd || exit
+    else
+        echo "$currdir is not a flutter project"
+    fi
+    
+done
+
+popd || exit
