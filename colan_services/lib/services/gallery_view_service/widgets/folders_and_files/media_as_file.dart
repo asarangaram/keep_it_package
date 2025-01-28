64352b851be58ab8d704a183fab0ba6e2286adfa
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-01-28 11:59:24 +0530

    remove unwanted imports

--- colan_services/lib/services/gallery_view_service/widgets/folders_and_files/media_as_file.dart
+++ colan_services/lib/services/gallery_view_service/widgets/folders_and_files/media_as_file.dart
@@ -4,7 +4,6 @@ import 'package:colan_services/colan_services.dart';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:content_store/content_store.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:store/store.dart';
 
 import '../../../media_view_service/media_view_service1.dart';
--- colan_services/lib/services/notes_service/widgets/text/text_note.dart
+++ colan_services/lib/services/notes_service/widgets/text/text_note.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:colan_widgets/colan_widgets.dart';
 import 'package:content_store/content_store.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:store/store.dart';
 
