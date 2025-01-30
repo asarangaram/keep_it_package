91b1d9375d15683985af8c12a323af84c29449ec
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-01-30 12:28:56 +0530

    warnings

--- colan_services/lib/services/gallery_view_service/widgets/actions/top_bar.dart
+++ colan_services/lib/services/gallery_view_service/widgets/actions/top_bar.dart
@@ -3,10 +3,8 @@ import 'package:content_store/content_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keep_it_state/keep_it_state.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:store/store.dart';
 
-import '../../../../internal/entity_grid/providers/tap_state.dart';
 import '../../../../internal/entity_grid/widgets/selection_control/selection_control.dart';
 import '../popover_menu.dart';
 
