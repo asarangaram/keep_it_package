15035276143b28486a71cadce2ef6792a0d63f72
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-04-09 20:54:51 +0530

    refactor: remove unused Filter extension and clean up list.dart

--- colan_services/lib/extensions/list.dart
+++ colan_services/lib/extensions/list.dart
@@ -1,5 +1,3 @@
-import 'package:store/store.dart';
-
 extension IndexExtonList<T> on List<T> {
   List<T> replaceNthEntry(int index, T newValue) {
     if (index < 0 || index >= length) {
@@ -11,60 +9,5 @@ extension IndexExtonList<T> on List<T> {
       newValue, // New value at the index
       ...sublist(index + 1), // Elements after the index
     ];
-  }
-}
-
-extension Filter on List<CLEntity> {
-  Map<String, List<CLEntity>> filterByDate() {
-    final filterredMedia = <String, List<CLEntity>>{};
-    final noDate = <CLEntity>[];
-    for (final entry in this) {
-      final String formattedDate;
-      if (entry.createDate != null) {
-        formattedDate = entry.createDate!.toDisplayFormat(dataOnly: true);
-      } else {
-        formattedDate = '${entry.addedDate.toDisplayFormat(dataOnly: true)} '
-            '(upload date)';
-      }
-      if (!filterredMedia.containsKey(formattedDate)) {
-        filterredMedia[formattedDate] = [];
-      }
-      filterredMedia[formattedDate]!.add(entry);
-    }
-    if (noDate.isNotEmpty) {
-      filterredMedia['No Date'] = noDate;
-    }
-
-    return filterredMedia;
-  }
-
-  List<GalleryGroupStoreEntity<CLEntity>> groupByDate() {
-    final galleryGroups = <GalleryGroupStoreEntity<CLEntity>>[];
-    for (final entry in filterByDate().entries) {
-      if (entry.value.length > 20) {
-        final groups = entry.value.convertTo2D(20);
-
-        for (final (index, group) in groups.indexed) {
-          galleryGroups.add(
-            GalleryGroupStoreEntity(
-              group,
-              label: (index == 0) ? entry.key : null,
-              groupIdentifier: entry.key,
-              chunkIdentifier: '${entry.key} $index',
-            ),
-          );
-        }
-      } else {
-        galleryGroups.add(
-          GalleryGroupStoreEntity(
-            entry.value,
-            label: entry.key,
-            groupIdentifier: entry.key,
-            chunkIdentifier: entry.key,
-          ),
-        );
-      }
-    }
-    return galleryGroups;
   }
 }
