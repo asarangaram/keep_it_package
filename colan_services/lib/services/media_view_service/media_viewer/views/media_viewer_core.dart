0b5c138f22456bb98344762ba3b9b2f7eb5430ae
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-04-29 02:26:54 +0530

    start playing video

--- colan_services/lib/services/media_view_service/media_viewer/views/media_viewer_core.dart
+++ colan_services/lib/services/media_view_service/media_viewer/views/media_viewer_core.dart
@@ -1,4 +1,5 @@
 import 'package:cl_entity_viewers/cl_entity_viewers.dart';
+import 'package:cl_media_tools/cl_media_tools.dart';
 import 'package:cl_media_viewers_flutter/cl_media_viewers_flutter.dart';
 import 'package:content_store/content_store.dart';
 
@@ -22,62 +23,107 @@ class MediaViewerCore extends StatelessWidget {
       (state) => state.length,
     );
 
-    return ListenableBuilder(
-      listenable: uiStateManager.notifier,
-      builder: (_, __) {
-        return switch (length.value) {
-          0 => Container(),
-          1 => ViewMedia(
-              currentItem: currentItem.value,
-              parentIdentifier: parentIdentifier,
-              autoStart: true,
-            ),
-          _ => MediaViewerPageView(
-              parentIdentifier: parentIdentifier,
-            )
-        };
+    return GetVideoPlayerControls(
+      builder: (controls) {
+        return ListenableBuilder(
+          listenable: uiStateManager.notifier,
+          builder: (_, __) {
+            return switch (length.value) {
+              0 => Container(),
+              1 => ViewMedia(
+                  currentItem: currentItem.value,
+                  parentIdentifier: parentIdentifier,
+                  autoStart: true,
+                  playerControls: controls,
+                ),
+              _ => MediaViewerPageView(
+                  parentIdentifier: parentIdentifier,
+                  playerControls: controls,
+                )
+            };
+          },
+        );
       },
     );
   }
 }
 
-class ViewMedia extends StatelessWidget {
+class ViewMedia extends StatefulWidget {
   const ViewMedia({
     required this.currentItem,
     required this.parentIdentifier,
+    required this.playerControls,
     super.key,
     this.autoStart = false,
   });
   final String parentIdentifier;
   final ViewerEntityMixin currentItem;
+  final VideoPlayerControls playerControls;
   final bool autoStart;
 
   @override
+  State<ViewMedia> createState() => _ViewMediaState();
+}
+
+class _ViewMediaState extends State<ViewMedia> {
+  @override
+  void initState() {
+    //setVideo();
+    super.initState();
+  }
+
+  Future<void> setVideo() async {
+    // This seems still required
+    await widget.playerControls.setVideo(
+      widget.currentItem.mediaUri!,
+      forced: false,
+      autoPlay: true,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    print('autoStart = $autoStart');
+    print('autoStart = ${widget.autoStart}');
 
     return ListenableBuilder(
       listenable: uiStateManager.notifier,
       builder: (_, __) {
         final stateManager = uiStateManager.notifier.state;
         final currentItem = stateManager.currentItem;
-        return Container(
-          padding: EdgeInsets.zero,
-          alignment: Alignment.center,
-          child: MediaViewer(
-            heroTag: '$parentIdentifier /item/${currentItem.id}',
-            uri: currentItem.mediaUri!,
-            previewUri: currentItem.previewUri,
-            mime: (currentItem as StoreEntity).data.mimeType!,
-            onLockPage: ({required bool lock}) {},
-            isLocked: false,
-            autoStart: autoStart,
-            autoPlay: true, // Fixme
-            errorBuilder: (_, __) => const BrokenImage(),
-            loadingBuilder: () => const GreyShimmer(),
-            keepAspectRatio: true,
-            hasGesture: !stateManager.showMenu,
-          ),
+        final isPlayable = widget.autoStart &&
+            widget.playerControls.uri != widget.currentItem.mediaUri &&
+            widget.currentItem.mediaType == CLMediaType.video &&
+            widget.currentItem.mediaUri != null;
+        return GetVideoPlayerControls(
+          builder: (controls) {
+            return Container(
+              padding: EdgeInsets.zero,
+              alignment: Alignment.center,
+              child: GestureDetector(
+                onTap: () async {
+                  await setVideo();
+                  await controls.onPlayPause(
+                    autoPlay: false,
+                    forced: isPlayable,
+                  );
+                },
+                child: MediaViewer(
+                  heroTag: '${widget.parentIdentifier} /item/${currentItem.id}',
+                  uri: currentItem.mediaUri!,
+                  previewUri: currentItem.previewUri,
+                  mime: (currentItem as StoreEntity).data.mimeType!,
+                  onLockPage: ({required bool lock}) {},
+                  isLocked: false,
+                  autoStart: widget.autoStart,
+                  autoPlay: true, // Fixme
+                  errorBuilder: (_, __) => const BrokenImage(),
+                  loadingBuilder: () => const GreyShimmer(),
+                  keepAspectRatio: true,
+                  hasGesture: !stateManager.showMenu,
+                ),
+              ),
+            );
+          },
         );
       },
     );
@@ -85,8 +131,13 @@ class ViewMedia extends StatelessWidget {
 }
 
 class MediaViewerPageView extends ConsumerStatefulWidget {
-  const MediaViewerPageView({required this.parentIdentifier, super.key});
+  const MediaViewerPageView({
+    required this.parentIdentifier,
+    required this.playerControls,
+    super.key,
+  });
   final String parentIdentifier;
+  final VideoPlayerControls playerControls;
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -101,6 +152,7 @@ class _MediaViewerPageViewState extends ConsumerState<MediaViewerPageView> {
     pageController = PageController(
       initialPage: uiStateManager.notifier.state.currentIndex,
     );
+
     super.initState();
   }
 
@@ -132,6 +184,7 @@ class _MediaViewerPageViewState extends ConsumerState<MediaViewerPageView> {
               parentIdentifier: widget.parentIdentifier,
               currentItem: stateManager.entities[index],
               autoStart: index == stateManagerNotifier.state.currentIndex,
+              playerControls: widget.playerControls,
             );
           },
         );
