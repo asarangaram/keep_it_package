d3b25434867f606f0049050d0f91489395e6f3bf
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-06-13 12:46:58 +0530

    dont expose Incoming Media Server to main

--- colan_services/lib/colan_services.dart
+++ colan_services/lib/colan_services.dart
@@ -6,8 +6,6 @@ export 'services/app_start_service/views/app_start_service.dart'
 export 'services/camera_service/camera_service.dart' show CameraService;
 export 'services/entity_viewer_service/entity_viewer_service.dart'
     show EntityViewerService;
-export 'services/incoming_media_service/incoming_media_service.dart'
-    show IncomingMediaService;
 export 'services/media_wizard_service/media_wizard_service.dart'
     show MediaWizardService;
 export 'services/other_services/media_edit_service.dart' show MediaEditService;
