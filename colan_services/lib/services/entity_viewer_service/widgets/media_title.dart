8a74f07754360328b9c08019430e781ea26890c6
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-06-25 19:22:49 +0530

    show the store name blow title

--- colan_services/lib/services/entity_viewer_service/widgets/media_title.dart
+++ colan_services/lib/services/entity_viewer_service/widgets/media_title.dart
@@ -1,4 +1,5 @@
 import 'package:cl_entity_viewers/cl_entity_viewers.dart';
+import 'package:content_store/content_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
@@ -13,33 +14,53 @@ class MediaTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final defaultTitle = Text(
-      'Keep It',
-      style: ShadTheme.of(context).textTheme.h1,
-    );
-    return entityAsync.when(
-        loading: () => defaultTitle,
-        error: (e, st) => defaultTitle,
-        data: (entity) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (entity == null)
-                defaultTitle
-              else
-                Text(
-                  entity.label?.capitalizeFirstLetter() ??
-                      'media #${entity.id ?? "New Media"}',
-                  style: ShadTheme.of(context).textTheme.h3,
-                ),
-              if (entity != null)
-                Text(
-                  DateFormat('dd MMM, yyyy')
-                      .format(entity.createDate ?? entity.updatedDate),
-                  style: ShadTheme.of(context).textTheme.small,
-                ),
-            ],
+    return GetActiveStore(
+        loadingBuilder: () => const CustomListTile(title: 'Keep It'),
+        errorBuilder: (e, st) => const CustomListTile(title: 'Keep It'),
+        builder: (activeStore) {
+          final defaultTitle = CustomListTile(
+            title: 'Keep It',
+            subTitle: activeStore.label,
           );
+          return entityAsync.when(
+              loading: () => defaultTitle,
+              error: (e, st) => defaultTitle,
+              data: (entity) {
+                return Column(
+                  children: [
+                    if (entity == null)
+                      defaultTitle
+                    else
+                      CustomListTile(
+                        title: entity.label?.capitalizeFirstLetter() ??
+                            'media #${entity.id ?? "New Media"}',
+                        subTitle: activeStore.label,
+                      ),
+                  ],
+                );
+              });
         });
+  }
+}
+
+class CustomListTile extends ConsumerWidget {
+  const CustomListTile({required this.title, super.key, this.subTitle});
+  final String title;
+  final String? subTitle;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      title: Text(
+        title,
+        style: ShadTheme.of(context).textTheme.h3,
+      ),
+      subtitle: (subTitle == null)
+          ? null
+          : Text(
+              subTitle!,
+              style: ShadTheme.of(context).textTheme.small,
+            ),
+    );
   }
 }
