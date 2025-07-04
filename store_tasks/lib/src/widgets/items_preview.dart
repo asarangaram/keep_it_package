96ac2f70905bc235f85ca2ac214d08c5af3e77d3
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-07-04 10:43:14 +0530

    fix: handle empty media case gracefully in WizardPreview

--- store_tasks/lib/src/widgets/items_preview.dart
+++ store_tasks/lib/src/widgets/items_preview.dart
@@ -29,42 +29,42 @@ class _WizardPreviewState extends ConsumerState<WizardPreview> {
       final media0 = activeTask.itemsConfirmed == null
           ? activeTask.items
           : activeTask.currEntities(selectionMode: selectionMode);
-      if (media0.isEmpty) {
-        throw Exception('Nothing to show');
-      }
+
       return ClipRRect(
         borderRadius: const BorderRadius.only(
             bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
-        child: CLEntitiesGridView(
-          incoming: ViewerEntities(media0),
-          filtersDisabled: true,
-          whenEmpty: const Text('Nothing to show here'),
+        child: media0.isEmpty
+            ? const Center(child: Text('Nothing to show'))
+            : CLEntitiesGridView(
+                incoming: ViewerEntities(media0),
+                filtersDisabled: true,
+                whenEmpty: const Text('Nothing to show here'),
 
-          // Wizard don't use context menu
-          contextMenuBuilder: null,
-          onSelectionChanged: (items) => ref
-              .read(activeTaskProvider.notifier)
-              .selectedMedia = items.entities,
-          itemBuilder: (context, item, entities) {
-            final Widget widget;
-            if (item.isCollection) {
-              widget = LayoutBuilder(
-                builder: (context, constrain) {
-                  return Image.asset(
-                    'assets/icon/icon.png',
-                    width: constrain.maxWidth,
-                    height: constrain.maxHeight,
-                  );
+                // Wizard don't use context menu
+                contextMenuBuilder: null,
+                onSelectionChanged: (items) => ref
+                    .read(activeTaskProvider.notifier)
+                    .selectedMedia = items.entities,
+                itemBuilder: (context, item, entities) {
+                  final Widget widget;
+                  if (item.isCollection) {
+                    widget = LayoutBuilder(
+                      builder: (context, constrain) {
+                        return Image.asset(
+                          'assets/icon/icon.png',
+                          width: constrain.maxWidth,
+                          height: constrain.maxHeight,
+                        );
+                      },
+                    );
+                  } else {
+                    widget = MediaThumbnail(
+                      media: item,
+                    );
+                  }
+                  return widget;
                 },
-              );
-            } else {
-              widget = MediaThumbnail(
-                media: item,
-              );
-            }
-            return widget;
-          },
-        ),
+              ),
       );
     });
   }
